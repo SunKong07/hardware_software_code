dc9e8aa78c8458ba671ba21bf3b29aed71a34dbe
SunKong07 <155668900+SunKong07@users.noreply.github.com>
2024-01-08 14:37:17 -0500

    my first commit

---
+++ yourname.py
@@ -0,0 +1,9 @@
+def main():
+    name = input("What is your name?")
+    print("We want to now if you like programming?")
+    answer = input("Do you like programming "+ name + "?")
+    print('Great! You said "' + answer + '"!')
+    print("Let's learning some Python today")
+
+if __name__ == "__main__":
+    main()
