0f366dc9705afa19bc685007e88fe46147025ff8
SunKong07 <155668900+SunKong07@users.noreply.github.com>
2024-01-17 12:41:21 -0500

    20240117 H&S Class

--- yourconversation.py
+++ yourconversation.py
@@ -1,14 +1,13 @@
 def conversation():
     print("Do you like coding in python? Answer yes or no")
-    answer = input()
+    answer = input().lower().strip()
     if answer == "yes":
         print("That's good - the United States needs more coders!!")
     elif answer == "no":
         print("Perhaps you will change your mind ")
-        print("Goodbye")
     else:
         print("I don't understand?")
-
+        print("Goodbye")
 def main():
     print("Welcome to my conversation program")
     conversation()
